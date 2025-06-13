4ce412e76826fa82fdbb2add29d5fccdd684e5ca
K1ng0hat <adrianmolanonavarro@gmail.com>
2025-06-13 15:37:45 -0500

    Refactor app component and update index.html for improved structure and responsiveness

--- Frontend/contaflux/src/app/app.component.ts
+++ Frontend/contaflux/src/app/app.component.ts
@@ -13,12 +13,13 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Agregar', url: '/folder/agregar', icon: 'mail' }, // Agregar personas que deben
+    { title: 'Pendientes', url: '/folder/pending', icon: 'mail' }, // Pendientes de pago con tabla de checkbox
+    { title: 'Gente debiente', url: '/folder/gotagota', icon: 'paper-plane' },
+    { title: 'Pagos Finalizados', url: '/folder/finalizados', icon: 'heart' },
+    { title: 'Archivados', url: '/folder/archived', icon: 'archive' },
+
+
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {
